583ba9abce0c9ee83a69ebea66b41d4491c7f0e8
chanpe207 <chanpe207@gmail.com>
2024-03-25 10:53:10 +1100

    added task allocation cpp files

---
+++ taskallocation.cpp
@@ -0,0 +1,7 @@
+#include "taskallocation.h"
+
+TaskAllocation::TaskAllocation() {
+
+}
+
+
---
+++ taskallocation.h
@@ -0,0 +1,18 @@
+#ifndef TASKALLOCATION_H
+#define TASKALLOCATION_H
+
+// Include any other header files this class relies on
+
+class TaskAllocation 
+{
+public:
+    // Default Constructors
+    TaskAllocation();
+    
+protected:
+
+private:
+
+}
+
+#endif TASKALLOCATION_H
